2b98c80e3c90410f5a930f342c8a8749532fe5dc
fiLLLip <filiptomren@gmail.com>
2016-09-13 22:09:11 +0200

    New specifications with links.

--- src/main/java/net/brennheit/mcashapi/resource/PaymentRequestLink.java
+++ src/main/java/net/brennheit/mcashapi/resource/PaymentRequestLink.java
@@ -1,5 +1,5 @@
-/* 
- * Copyright (c) 2014, fiLLLip
+/*
+ * Copyright (c) 2016, fiLLLip
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,37 +32,12 @@ import java.util.List;
  *
  * @author fiLLLip
  */
-public class CreatePaymentRequest {
+public class PaymentRequestLink {
 
     @Key
-    public String ledger;
+    public String uri;
     @Key
-    public String callback_uri;
+    public String caption;
     @Key
-    public String display_message_uri;
-    @Key
-    public String customer;
-    @Key
-    public String currency;
-    @Key
-    public String amount;
-    @Key
-    public String additional_amount;
-    @Key
-    public boolean additional_edit;
-    @Key
-    public boolean allow_credit;
-    @Key
-    public String pos_id;
-    @Key
-    public String pos_tid;
-    @Key
-    public String text;
-    @Key
-    public String action;
-    @Key
-    public int expires_in;
-    @Key
-    public List<PaymentRequestLink> links;
-
+    public List<String> show_on;
 }
